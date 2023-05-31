d06e79569337cc96f806f8250514d81c33b300c2
Ivan Lupandin <vanyalupandin@gmail.com>
2023-05-31 22:26:41 +0300

    Init Skillfactory

---
+++ TellegramBot/BOT.py
@@ -0,0 +1,64 @@
+from config import *
+from extensions import Convertor, APIException
+
+import telebot
+from telebot import types
+
+conv_markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+buttons = []
+for val in exchanges.keys():
+    buttons.append(types.KeyboardButton(val.capitalize()))
+
+conv_markup.add(*buttons)
+
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def start(message: telebot.types.Message):
+    text = "Приветствие!"
+    bot.send_message(message.chat.id, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for i in exchanges.keys():
+        text = '\n'.join((text, i))
+    bot.send_message(message.chat.id, text)
+
+
+@bot.message_handler(commands=['convert'])
+def values(message: telebot.types.Message):
+    text = 'Выберите валюту, из которой конвертировать:'
+    bot.send_message(message.chat.id, text, reply_markup=conv_markup)
+    bot.register_next_step_handler(message, base_handler)
+
+
+def base_handler(message: telebot.types.Message):
+    base = message.text.strip()
+    text = 'Выберите валюту, в которую конвертировать:'
+    bot.send_message(message.chat.id, text, reply_markup=conv_markup)
+    bot.register_next_step_handler(message, sym_handler, base)
+
+
+def sym_handler(message: telebot.types.Message, base):
+    sym = message.text.strip()
+    text = 'Выберите количество конвертируемой валюты:'
+    bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(message, amount_handler, base, sym)
+
+
+def amount_handler(message: telebot.types.Message, base, sym):
+    amount = message.text.strip()
+    try:
+        new_price = Convertor.get_price(base, sym, amount)
+    except APIException as e:
+        bot.send_message(message.chat.id, f"Ошибка конвертации: \n{e}")
+    else:
+        text = f"Цена {amount} {base} в {sym} : {new_price}"
+        bot.send_message(message.chat.id, text)
+
+
+bot.polling()
---
+++ TellegramBot/config.py
@@ -0,0 +1,6 @@
+exchanges = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
+TOKEN = "6231855124:AAFqgOaHJhmo-LT-ys2-oOo9Ao16dW_cJMg"
